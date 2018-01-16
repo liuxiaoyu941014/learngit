f200be15055af83e76cc836fce2de202ce78b1e6
liuxiaoyu941014 <1804183321@qq.com>
2018-01-16 17:59:28 +0800

    第六天项目进度

--- lxy_wgtong/config/routes.rb
+++ lxy_wgtong/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/sign_up'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to =>"culture#index"
 end
