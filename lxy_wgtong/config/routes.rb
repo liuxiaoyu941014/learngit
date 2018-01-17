c928b1712a1d92c8b870d604f56babadc299bee4
liuxiaoyu941014 <1804183321@qq.com>
2018-01-17 18:15:37 +0800

    第七天进度

--- lxy_wgtong/config/routes.rb
+++ lxy_wgtong/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'users/sign_up'
+  get 'users/sign_in'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to =>"culture#index"
