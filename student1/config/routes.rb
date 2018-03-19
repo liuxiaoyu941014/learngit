cd65ff23f5b4e9b86be76d463f96e4b5cd631f8a
liuxiaoyu <liuxiaoyu@wgtong.cn>
2018-03-19 18:09:19 +0800

    报名系统

---
+++ student1/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'users/sign_in'
+  get 'students/index'
+  get 'students/table'
+  get 'students/table'
+  get 'students/data'
+  get 'students/forms'
+  get 'students/contact'
+  get 'students/blank'
+  get 'students/result'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
