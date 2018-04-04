291935b6686f808dbaf5066af00b62721c1ed18c
liuxiaoyu <liuxiaoyu@wgtong.cn>
2018-04-04 13:55:15 +0800

    市民图片修改

--- dagle/app/models/course.rb
+++ dagle/app/models/course.rb
@@ -1,4 +1,9 @@
 class Course < ApplicationRecord
   audited
-  
+  has_many :image_item_relations, as: :relation
+  has_many :image_items, :through => :image_item_relations
+  def first_image
+    image_items.first.try(:image_url) || 'http://song-dev.qiniudn.com/product.jpg'
+  end
+
 end
