a7a2ca7cb0da07daefbfbcf03254f38a4f2f68b4
liuxiaoyu <liuxiaoyu@wgtong.cn>
2018-04-08 09:42:52 +0800

    每人选择课程限制修改

--- dagle/app/controllers/frontend/courses_controller.rb
+++ dagle/app/controllers/frontend/courses_controller.rb
@@ -2,7 +2,7 @@ class Frontend::CoursesController < Frontend::BaseController
   #前台首页
   def index
     @news_channel = @cms_site.channels.find_by(short_title: 'index')
-    
+    @class = Classorder.all
     @course = Course.all
   end
   def show 
